a4148d46f787586612078dbd16cbd7c45a2a4268
apkqiu <apkqiu@qq.com>
2026-07-20 13:29:11 +0800

    添加版权和额外说明

---
+++ head.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name         QZone auto like
+// @namespace    http://tampermonkey.net/
+// @version      2026-07-19
+// @description  QQ空间自动点赞评论
+// @author       apkqiu
+// @match        https://user.qzone.qq.com/*
+// @icon         https://i.qq.com/favicon.ico
+// @grant        none
+// ==/UserScript==
